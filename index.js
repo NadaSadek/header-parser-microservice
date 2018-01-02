c20be0e6923dc48fd187d8c6cdcc27a50f936592
Nada Sadek <nadasadek92@gmail.com>
2018-01-02 15:17:02 +0200

    fix port + add html

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = 8000;
+const port = process.env.PORT || 8000;
 let myInfo = {};
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(express.static(__dirname + '/views'));
 
+app.get('/', function(req, res) {
+	res.sendFile('index.html');
+});
+
 app.get('/myinfo', function(req, res) {
-	myInfo.ipaddress = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	myInfo.ipaddress =  req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip;
 	myInfo.language = req.headers['accept-language'].split(',')[0];
-	myInfo.system = req.headers['user-agent'];
+	myInfo.system = req.headers['user-agent'].split('(')[1].split(')')[0];
 
 	console.log(req.headers);
     res.json(myInfo);
@@ -18,6 +22,6 @@ app.get('/myinfo', function(req, res) {
 });
 
 //listen for requests
-var listener = app.listen(PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
